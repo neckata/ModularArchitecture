d878e7210c334e46edb3041d4b85dbf3b4be28b2
Nikola Netsov <pikedbs@abv.bg>
2022-04-17 16:59:03 +0300

    Fix

--- Modules/ExcelUpload/ExcelUpload.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ExcelUpload.Core
-{
-    public class Class1
-    {
-    }
-}
--- Modules/ExcelUpload/Program.cs
+++ Modules/ExcelUpload/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ExcelUpload
+namespace ConsoleApp1
 {
     class Program
     {
