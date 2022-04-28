d0c673db72f02ab7760a90681b3aa2e20bf49e6d
Nikola Netsov <pikedbs@abv.bg>
2022-04-28 15:30:46 +0300

    Controllers

---
+++ Host/Gamification/Controllers/ExcelUpload/ExcelUploadController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using ExcelUpload.Infrastructure.Services;
+using Gamification.Shared.Core.Constants;
+using Gamification.Shared.DTOs.Connector;
+using Gamification.Shared.DTOs.Filters;
+using Gamification.Shared.Infrastructure.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gamification.Controllers.ExcelUpload
+{
+    [ApiVersion("1")]
+    public class ExcelUploadController : CommonBaseController
+    {
+        private IExcelUploadService _excelUploadService;
+
+        public ExcelUploadController(IExcelUploadService excelUploadService)
+        {
+            _excelUploadService = excelUploadService;
+        }
+
+        [HttpPut]
+        [Authorize(Policy = Permissions.ExcelUpload.Edit)]
+        public async Task<IActionResult> UploadFile()
+        {
+            return Ok(await _excelUploadService.UploadFile());
+        }
+    }
+}
---
+++ Modules/ExcelUpload/ExcelUpload.Core/Interfaces/IExcelUploadService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Gamification.Shared.Core.Wrapper;
+
+namespace ExcelUpload.Infrastructure.Services
+{
+    public interface IExcelUploadService
+    {
+        public Task<IResult<string>> UploadFile();
+    }
+}
